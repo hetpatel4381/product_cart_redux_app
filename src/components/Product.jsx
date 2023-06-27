44320dee87692a0d5d16ffa49739ac05b738d249
hetpatel4381 <hetpatel2808hp@gmail.com>
2023-06-27 23:20:12 +0530

    API call is completed using old react method

---
+++ src/components/Product.jsx
@@ -0,0 +1,19 @@
+import { useState, useEffect } from "react";
+
+const Product = () => {
+    const [products, getProducts] = useState([]);
+
+    useEffect(() => {
+        //api
+        fetch("https://fakestoreapi.com/products").then(data => data.json()).then(result => getProducts(result));
+    }, [])
+
+    return (
+      <div>
+        <h1>Product Dashboard</h1>
+        {JSON.stringify(products)}
+      </div>
+    );
+}
+
+export default Product;
